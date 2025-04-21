1c5307e7855f0e6627f72db025caa70bd44d1385
Daniel Lima <137017200+DanielGLima@users.noreply.github.com>
2025-04-20 22:59:32 -0600

    actualizacion

--- src/main/java/gestionTickets/Administrador.java
+++ src/main/java/gestionTickets/Administrador.java
@@ -1,5 +1,7 @@
 package gestionTickets;
 
+import java.util.List;
+
 public class Administrador extends Persona {
 
     public Administrador(String idPersona, String nombre, String correo, String telefono, Rol rol) {
@@ -7,9 +9,21 @@ public class Administrador extends Persona {
     }
 
 
-    public void crearDepartamento(Departamento departamento) {
-        
+    public void crearDepartamento(String nombre, String descripcion, List<Departamento> departamentos) {
+        if (nombre.isEmpty() || descripcion.isEmpty()) {
+            System.out.println("El nombre o la descripcion no puede estar vacio");
 
+        } else {
+            for (Departamento departamento : departamentos) {
+                if (departamento.getNombre().equalsIgnoreCase(nombre)) {
+                    System.out.println("El departamento ya existe");
+                    return;
+                }
+            }
+        }
+        Departamento departamento = new Departamento(null, nombre, descripcion);
+        departamentos.add(departamento);
+        System.out.println("Departamento agregado");
 
     }
 }
